80b87ed026d1b197969e831258823dfb6a6162ef
coral <357437579@qq.com>
2024-09-13 14:36:30 +0800

    build param

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -2,5 +2,9 @@ import commonConfig from './vite.config.common';
 
 export default {
     ...commonConfig,
-    base: '/push-box/push-box/dist/',
+    build: {
+        outDir:'./website',
+        emptyOutDir: true,
+    },
+    base: '/',
 };
